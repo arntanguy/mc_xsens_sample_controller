7afe8015cb0867e338dcab567f27841547eeaf3e
Arnaud Tanguy <arn.tanguy@gmail.com>
2021-12-10 17:15:01 +0100

    Fully functional sample for JVRC1
    - Define mapping between links/segments orientation

--- src/states/XsensRetargetting.cpp
+++ src/states/XsensRetargetting.cpp
@@ -17,17 +17,50 @@ void XsensRetargetting::start(mc_control::fsm::Controller & ctl_)
   {
     mc_rtc::log::error_and_throw<std::runtime_error>("[{}] This state requires the XsensPlugin", name());
   }
-  bodyToSegment_ = ctl.config()("Xsens")(ctl.robot().name());
+  
+  auto robotConfig = static_cast<std::map<std::string, mc_rtc::Configuration>>(ctl.config()("Xsens")(ctl.robot().name()));
+  for(const auto & bodyConfig : robotConfig)
+  {
+    const auto & bodyName = bodyConfig.first;
+    const auto & bodyConf = bodyConfig.second;
+    bodyConfigurations_[bodyName] = XsensBodyConfiguration{};
+    auto & bodyC = bodyConfigurations_[bodyName];
+    bodyC.bodyName = bodyName;
+    bodyC.segmentName = static_cast<std::string>(bodyConf("segment"));
+    bodyConf("offset", bodyC.offset);
+    ctl.gui()->addElement({"Xsens", "Bodies", bodyName},
+      mc_rtc::gui::ArrayInput("Offset translation [m]",
+      [&bodyC]()
+      {
+        return bodyC.offset.translation();
+      },
+      [&bodyC](const Eigen::Vector3d & offset)
+      {
+        bodyC.offset.translation() = offset;
+      }),
+      mc_rtc::gui::ArrayInput("Offset rotation [rad]",
+      [&bodyC]()
+      {
+        return mc_rbdyn::rpyFromMat(bodyC.offset.rotation());
+      },
+      [&bodyC](const Eigen::Vector3d & offset)
+      {
+        bodyC.offset.rotation() = mc_rbdyn::rpyToMat(offset);
+      })
+    );
+  }
 
-  for(const auto & body: bodyToSegment_)
+  // Initialize tasks
+  for(auto & body: bodyConfigurations_)
   {
     const auto & bodyName = body.first;
-    const auto & segmentName = body.second;
-    mc_rtc::log::info("body: {}, segment: {}", bodyName, segmentName);
+    const auto & segmentName = body.second.bodyName;
     if(ctl.robot().hasBody(bodyName))
     {
-      tasks_[bodyName] = std::unique_ptr<mc_tasks::EndEffectorTask>(new mc_tasks::EndEffectorTask(bodyName, ctl.robots(), ctl.robots().robotIndex(), stiffness_, weight_));
-      ctl.solver().addTask(tasks_[bodyName].get());
+      auto task = std::unique_ptr<mc_tasks::EndEffectorTask>(new mc_tasks::EndEffectorTask(bodyName, ctl.robots(), ctl.robots().robotIndex(), stiffness_, weight_));
+      task->reset();
+      ctl.solver().addTask(task.get());
+      tasks_[bodyName] = std::move(task); 
     }
     else
     {
@@ -40,16 +73,16 @@ void XsensRetargetting::start(mc_control::fsm::Controller & ctl_)
 bool XsensRetargetting::run(mc_control::fsm::Controller & ctl_)
 {
   auto & ctl = static_cast<XsensSampleController &>(ctl_);
-  for(const auto & body: bodyToSegment_)
+  for(const auto & body: bodyConfigurations_)
   {
     const auto & bodyName = body.first;
-    const auto & segmentName = body.second;
+    const auto & segmentName = body.second.segmentName;
     if(ctl.robot().hasBody(bodyName))
     {
       try
       {
-        const auto & segmentPose = ctl.datastore().call<sva::PTransformd>("XsensPlugin::GetSegmentPose", segmentName); 
-        tasks_[bodyName]->set_ef_pose(segmentPose); 
+        const auto segmentPose = ctl.datastore().call<sva::PTransformd>("XsensPlugin::GetSegmentPose", segmentName); 
+        tasks_[bodyName]->set_ef_pose(body.second.offset * segmentPose); 
       }
       catch(...)
       {
--- src/states/XsensRetargetting.h
+++ src/states/XsensRetargetting.h
@@ -3,9 +3,15 @@
 #include <mc_control/fsm/State.h>
 #include <mc_tasks/EndEffectorTask.h>
 
+struct XsensBodyConfiguration
+{
+    std::string segmentName{};
+    std::string bodyName{};
+    sva::PTransformd offset = sva::PTransformd::Identity();
+};
+
 struct XsensRetargetting : mc_control::fsm::State
 {
-
     void configure(const mc_rtc::Configuration & config) override;
 
     void start(mc_control::fsm::Controller & ctl) override;
@@ -14,8 +20,8 @@ struct XsensRetargetting : mc_control::fsm::State
 
     void teardown(mc_control::fsm::Controller & ctl) override;
 private:
-    std::map<std::string, std::string> bodyToSegment_;
+    std::map<std::string, XsensBodyConfiguration> bodyConfigurations_;
     std::map<std::string, std::unique_ptr<mc_tasks::EndEffectorTask>> tasks_;
-    double stiffness_ = 2;
+    double stiffness_ = 10;
     double weight_ = 1000;
 };
